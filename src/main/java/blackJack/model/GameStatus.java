24c9a4d48c3200835ebd3a47df621e798ad7a858
JavierDolo <doralozier@gmail.com>
2025-09-28 18:39:26 +0200

    initialize spring boot and create package model and its classes

---
+++ src/main/java/blackJack/model/GameStatus.java
@@ -0,0 +1,5 @@
+package blackJack.model;
+
+public enum GameStatus {
+    PLAYER_TURN, DEALER_TURN, FINISHED
+}
---
+++ src/main/java/blackJack/model/Outcome.java
@@ -0,0 +1,5 @@
+package blackJack.model;
+
+public enum Outcome {
+    PLAYER_BLACKJACK, PLAYER_WIN, DEALER_WIN, PUSH
+}
