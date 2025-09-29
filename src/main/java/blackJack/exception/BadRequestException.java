475a719a3f4f86193173fa7f1ae0c576ae25fcf8
JavierDolo <doralozier@gmail.com>
2025-09-29 12:08:41 +0200

    created exceptions

---
+++ src/main/java/blackJack/exception/BadRequestException.java
@@ -0,0 +1,5 @@
+package blackJack.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) { super(message); }
+}
---
+++ src/main/java/blackJack/exception/NotFoundException.java
@@ -0,0 +1,5 @@
+package blackJack.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) { super(message); }
+}
